d6549bc100c60f392cef627ef4dbc4935b585980
陈克欣 <18850262552@163.com>
2023-04-27 15:03:15 +0800

    设置跨域

--- frontend/controllers/BaseController.php
+++ frontend/controllers/BaseController.php
@@ -1,13 +1,6 @@
 <?php
 
 namespace frontend\controllers;
-header("Access-Control-Allow-Origin: *");
-//如果需要设置允许所有域名发起的跨域请求，可以使用通配符 * ，如果限制自己的域名的话写自己的域名就行了。
-// 响应类型 *代表通配符，可以指出POST,GET等固定类型
-header('Access-Control-Allow-Methods:* ');
-// 响应头设置
-header('Access-Control-Allow-Headers:x-requested-with,content-type');
-header('Access-Control-Allow-Credentials: true');
 
 use yii\web\Controller;
 use yii\web\Response;
@@ -44,4 +37,14 @@ class BaseController extends Controller
         return $response;
     }
 
+    public function allowOrigin()
+    {
+        header("Access-Control-Allow-Origin: *");
+        //如果需要设置允许所有域名发起的跨域请求，可以使用通配符 * ，如果限制自己的域名的话写自己的域名就行了。
+        // 响应类型 *代表通配符，可以指出POST,GET等固定类型
+        header('Access-Control-Allow-Methods:* ');
+        // 响应头设置
+        header('Access-Control-Allow-Headers:x-requested-with,content-type');
+        header('Access-Control-Allow-Credentials: true');
+    }
 }
